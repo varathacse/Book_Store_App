a622ec4b2b26cf1673667fb34e451dcb7e63f77d
varathacse <113516425+varathacse@users.noreply.github.com>
2024-08-15 12:43:07 +0530

    Add files via upload

---
+++ Backend Spring-Boot/src/main/java/com/bookstore/validate/BookValidate.java
@@ -0,0 +1,29 @@
+package com.bookstore.validate;
+
+import org.springframework.stereotype.Component;
+
+import com.bookstore.entities.Book;
+import com.bookstore.exception.FieldNotFoundException;
+
+@Component
+public class BookValidate {
+
+	public void validateBook(Book book) {
+
+		if (book.getBookName() == null) {
+			throw new FieldNotFoundException("Book Name is null");
+		}
+		if (book.getEdition() == null) {
+			throw new FieldNotFoundException("Book Edition is null");
+		}
+		if (book.getPrice() == null) {
+			throw new FieldNotFoundException("Book Price is null");
+		}
+		if (book.getDateOfPublish() == null) {
+			System.out.println(book.toString());
+			throw new FieldNotFoundException("Book Year of publlish is null");
+		}
+
+	}
+
+}
